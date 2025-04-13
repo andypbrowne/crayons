efd55430970c906b09a23459c9c0e88c93fd8c44
andypbrowne <andypbrowne@gmail.com>
2025-04-13 15:33:33 -0400

    add the files for data again

---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+module.exports = {
+	"title": "Andypbrowne",
+	"url": "https://crayons.andypbrowne.com/",
+	"language": "en",
+	"description": "Curated color palettes for creative projects",
+	"author": {
+		"name": "Andrew Garber-Browne",
+		"url": "https://andypbrowne.com/about/"
+	}
+}
